087992fdbc2fe2e98d3470a916ce92b74c9b2454
ZambranoGamer2002 <zambranogamerplay@gmail.com>
2023-07-03 01:28:36 -0500

    Subiendo Diseños, login, register

--- script/script.js
+++ script/script.js
@@ -12,11 +12,55 @@ loadMoreBtn.onclick = () => {
     }
 }
 
+let loadMoreBtn2 = document.querySelector('#load-more-2');
+let currentItem2 = 4;
 
-//carrito
+loadMoreBtn2.onclick = () => {
+    let boxes = [...document.querySelectorAll('.box-container-2 .box-2')];
+    for(var i = currentItem2; i< currentItem2 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem2 +=4;
+    if(currentItem2 >= boxes.length) {
+        loadMoreBtn2.style.display ='none'
+    }
+}
+
+
+let loadMoreBtn3 = document.querySelector('#load-more-3');
+let currentItem3 = 4;
+
+loadMoreBtn3.onclick = () => {
+    let boxes = [...document.querySelectorAll('.box-container-3 .box-3')];
+    for(var i = currentItem3; i< currentItem3 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem3 +=4;
+    if(currentItem3 >= boxes.length) {
+        loadMoreBtn3.style.display ='none'
+    }
+}
+
+let loadMoreBtn4 = document.querySelector('#load-more-4');
+let currentItem4 = 4;
+
+loadMoreBtn4.onclick = () => {
+    let boxes = [...document.querySelectorAll('.box-container-4 .box-4')];
+    for(var i = currentItem4; i< currentItem4 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem4 +=4;
+    if(currentItem4 >= boxes.length) {
+        loadMoreBtn4.style.display ='none'
+    }
+}
+
 
 const carrito = document.getElementById('carrito');
 const elementos1 = document.getElementById('lista-1');
+const elementos2 = document.getElementById('lista-2');
+const elementos3 = document.getElementById('lista-3');
+const elementos4 = document.getElementById('lista-4');
 const lista = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
 
@@ -24,6 +68,10 @@ cargarEventListeners();
 
 function cargarEventListeners() {
     elementos1. addEventListener('click', comprarElemento);
+    elementos2. addEventListener('click', comprarElemento);
+    elementos3. addEventListener('click', comprarElemento);
+    elementos4. addEventListener('click', comprarElemento);
+    
     carrito.addEventListener('click', eliminarElemento);
     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
 }
